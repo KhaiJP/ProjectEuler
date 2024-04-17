d213a40bb3a0921fa886f484e16e2e7826198d12
Khai <storage27592@gmail.com>
2024-04-17 12:45:52 +0900

    changed function names

--- <100/004/004.hs
+++ <100/004/004.hs
@@ -1,9 +1,7 @@
-main = print $ answer
-
--- functions
-reverseShow    = reverse . show
-isPalindrome n = (show n) == (reverseShow n)
+main = print $ maximum palindromes
 
 
-palindromes = [x*y | x <- [100..999], y <- [100..999], isPalindrome $ x*y]
-answer = maximum palindromes
+palindromes        = filter isPalindrome [x*y | x <- [100..999], y <- [100..999]]
+isPalindrome       = areSameValueAt show reverseShow
+reverseShow        = reverse . show
+areSameValueAt f g = \x -> (==) (f x) (g x)
