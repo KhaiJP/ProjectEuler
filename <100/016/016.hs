b5c83cdf2e9408a2bdd9b2cf0651f6d15b90c2db
Khai <storage27592@gmail.com>
2024-04-18 21:02:09 +0900

    fixed typo

--- <100/016/016.hs
+++ <100/016/016.hs
@@ -3,8 +3,8 @@ main = print $ digitSum pow
 digitSum :: Integer -> Integer
 digitSum 0 = 0
 digitSum n = addFirstDigit $ digitSum $ div n 10
-    where addFirstDigit = addGenerator' $ mod n 10
+    where addFirstDigit = addGenerator $ mod n 10
 
-addGenerator' = \x -> (+x)
-pow :: Integer = base ^ 1000
+addGenerator = \x -> (+x)
+pow :: Integer  = base ^ 1000
 base :: Integer = 2
