7aef3b79ce4d3e7edd8be2f70689eeae3c28b8fe
Khai <storage27592@gmail.com>
2024-04-23 09:37:54 +0900

    Solved 072 in C++

--- <100/072/072.cpp
+++ <100/072/072.cpp
@@ -1,30 +1,22 @@
 #include<iostream>
-#include<set>
 #include<numeric>
-#include<time.h>
 using namespace std;
 
 
 int main(){
-  clock_t start = clock();
-
-  set<pair<int, int>> S;
   const int max_d = 12000;
+  int answer = 0;
 
-  // naive brute-force; takes 5 secs, not super fast but enough
+  // naive brute-force;
   for(int d = max_d; d > 2; --d){
     // n/d < 1/2  <==> n < d/2  ==> max_n = d/2 - 1if d%2 == 0 else d/2.
     int n = d/2 - 1 + (d%2);
     while(3*n > d){
-      if(gcd(n, d) == 1) S.insert({n, d});
+      answer += gcd(n, d) == 1;
       --n;
     }
   }
 
-  clock_t end = clock();
-  const double time = static_cast<double>(end - start) / CLOCKS_PER_SEC;
-
-  cout << S.size() << endl;
-  cout << time << " sec" << endl;
+  cout << answer << endl;
   return 0;
 }
