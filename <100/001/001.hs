57657d62a4f6b961b65848ef9c724cfb47233b27
KhaiJP <75838028+KhaiJP@users.noreply.github.com>
2024-04-24 07:41:51 +0900

    Update 001.hs

--- <100/001/001.hs
+++ <100/001/001.hs
@@ -1,8 +1,1 @@
-main = print $ sum' l
-
-l = [1..999]
-sum' :: []Int -> Int
-sum' [] = 0
-sum' (x:xs)
-  |  mod x 3 == 0 || mod x 5 == 0 = x + sum' xs
-  |  otherwise = sum' xs
+main = print $ sum [n | n <- [1..999], n`mod`3 == 0 || n`mod`5 == 0]
