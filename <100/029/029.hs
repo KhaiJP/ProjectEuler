ed91a68a893b3d1fa98067de27a83de78bc51c59
Khai <storage27592@gmail.com>
2024-04-30 10:14:05 +0900

    Solved 029 in Haskell

---
+++ <100/029/029.hs
@@ -0,0 +1,12 @@
+import qualified Data.Set as S
+
+
+main = print $ count 2 2 S.empty
+
+count :: Integer -> Integer -> S.Set Integer -> Int
+count a b s
+    | a == 101  = S.size s
+    | b == 100  = count (a+1) 2 t
+    | otherwise = count a (b+1) t
+    where
+        t = S.insert (a^b) s
