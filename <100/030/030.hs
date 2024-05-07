4a5e316e1cc1191601561c18fac3a2b11a00b0b4
Khai <storage27592@gmail.com>
2024-05-07 19:25:19 +0900

    Updated 030s

--- <100/030/030.hs
+++ <100/030/030.hs
@@ -1,6 +1,6 @@
 main = print $ sum [n | n <- [2..upperLimit], isOK n]
 
-isOK = \n -> n == digit5thPowerSum n
+isOK n = n == digit5thPowerSum n
 
 digit5thPowerSum :: Int -> Int
 digit5thPowerSum 0 = 0
@@ -10,7 +10,7 @@ digit5thPowerSum n = a^5 + digit5thPowerSum m
         m = div n 10
 
 -- let us consider if there exists an upper limit of the number enjoying the condition.
--- the maximum m-digit number is apprently m * 9^5 < 60000m. to enjoy the condition,
--- this number must be less than 10^m (note 10^m is m+1-digit number).
--- for m = 6, 60000m = 360,000 > 1,000,000 = 10^m -> no number greater than 360000 enjoy the condition. 
+-- the maximum m-digit number is apprently m * 9^5 = 59049m bu< 60000m. to enjoy the condition,
+-- this number must be greater than or equal to 10^(m-1) (note 10^m is m+1-digit number).
+-- for m = 7, 60000m = 420,000 < 1,000,000 = 10^(m-1) -> m <= 6 -> no number greater than 360000 enjoy the condition. 
 upperLimit = 360000
--- <100/030/030.jl
+++ <100/030/030.jl
@@ -1,7 +1,7 @@
 # let us consider if there exists an upper limit of the number enjoying the condition.
-# the maximum m-digit number is apprently m * 9^5 < 60000m. to enjoy the condition,
-# this number must be less than 10^m (note 10^m is m+1-digit number).
-# for m = 6, 60000m = 360,000 > 1,000,000 = 10^m -> no number greater than 360000 enjoy the condition. 
+# the maximum m-digit number is apprently m * 9^5 = 59049m bu< 60000m. to enjoy the condition,
+# this number must be greater than or equal to 10^(m-1) (note 10^m is m+1-digit number).
+# for m = 7, 60000m = 420,000 < 1,000,000 = 10^(m-1) -> m <= 6 -> no number greater than 360000 enjoy the condition.
 
 function is_ok(n:: Int):: Bool
   power:: Int = 5
