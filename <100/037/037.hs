bce49ef6fb0124b6761224eb4edee55e551dc8a9
Khai <storage27592@gmail.com>
2024-05-07 21:17:08 +0900

    Solved 037 in Haskell

---
+++ <100/037/037.hs
@@ -0,0 +1,27 @@
+import Data.Numbers.Primes
+
+
+main :: IO ()
+main = print $ sum . take target $ [n | n <- [11..], all isPrime (family n)]
+
+
+family :: Int -> [Int]
+family n = [n] ++ lefts ++ rights
+    where
+        lefts  = map read . lefts' . show $ n
+        rights = map read . rights' . show $ n
+
+
+lefts' :: String -> [String]
+lefts' [c]  = []
+lefts' (c:cs) = cs : lefts' cs
+
+
+rights' :: String -> [String]
+rights' [c] = []
+rights' s = s' : rights' s'
+    where s' = init s
+
+
+target :: Int
+target = 11
