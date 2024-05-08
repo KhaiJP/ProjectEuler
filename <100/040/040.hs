7c24d463b730e4bca9a4f380d845f322f2c24a78
Khai <storage27592@gmail.com>
2024-05-09 07:56:53 +0900

    Solved 040 in Haskell

---
+++ <100/040/040.hs
@@ -0,0 +1,27 @@
+import Data.Char
+import Data.List
+
+
+main :: IO ()
+main = print $ product $ zipWith extract s [1..10^upperLimit]
+
+
+s :: String
+s = intercalate "" [show n | n <- [1..]]
+
+
+extract :: (Num t2, Ord t2) => Char -> t2 -> Int
+extract c n
+    | powerOf10 n 0 = digitToInt c
+    | otherwise     = 1
+
+
+powerOf10 :: (Integral t1, Num t2, Ord t2) => t2 -> t1 -> Bool
+powerOf10 n p
+    | 10^p > n  = False
+    | 10^p == n = True
+    | otherwise = powerOf10 n (p+1)
+
+
+upperLimit :: Int
+upperLimit = 6
