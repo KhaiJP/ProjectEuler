c016c7f28cb5382100df713478a7a52354bd9ae7
Khai <storage27592@gmail.com>
2024-05-10 23:50:41 +0900

    Solved 049 in Haskell

---
+++ <100/049/049.hs
@@ -0,0 +1,15 @@
+import Data.Numbers.Primes ( isPrime )
+import Data.List.Unique ( uniq )
+import Data.List ( sort )
+
+
+main :: IO ()
+main = putStrLn $ last . take 2 $ [concatMap show seq | a <- [1001..9999], d <- [2,4..(10000-2*a)], let seq = [a, a+d, a+2*d], check seq]
+
+
+check :: [Int] -> Bool
+check seq = allPrime && allPerms
+    where
+        allPrime = all isPrime seq
+        allPerms = (==) 1 length'
+        length'  = length . uniq  . map (sort . show) $ seq
