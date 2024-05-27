8feea14dbb955da5f58a7ea6081a26e5df43b2d4
Khai <storage27592@gmail.com>
2024-05-27 21:46:40 +0900

    Update 026.hs

--- <100/026/026.hs
+++ <100/026/026.hs
@@ -2,16 +2,16 @@ import Data.List
 
 
 main :: IO ()
-main = print $ (1+) <$> maximumAt [ length $ findCycle n | n <- [1..upperLimit]]
+main = print $ (1+) <$> maximumAt [ findPeriod n | n <- [1..upperLimit]]
 
 
-findCycle :: Integral t => t -> [t]
-findCycle = findCycle' 1 [] 
-findCycle' :: Integral t => t -> [t] -> t -> [t]
-findCycle' n rs m
-  | r == 0      = []
-  | r `elem` rs = dropWhile (/=r) $ reverse rs
-  | otherwise   = findCycle' n' rs' m
+findPeriod :: Integral t => t -> Int
+findPeriod = findPeriod' 1 [] 
+findPeriod' :: Integral t => t -> [t] -> t -> Int
+findPeriod' n rs m
+  | r == 0      = 0
+  | r `elem` rs = length . dropWhile (/=r) $ reverse rs
+  | otherwise   = findPeriod' n' rs' m
   where
     r   = n `mod` m
     n'  = r * 10
