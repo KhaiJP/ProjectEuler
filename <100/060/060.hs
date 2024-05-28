0c108b53573ec35646ebcd0ea6028826c86f0632
Khai <storage27592@gmail.com>
2024-05-29 01:06:03 +0900

    Update 060.hs

--- <100/060/060.hs
+++ <100/060/060.hs
@@ -1,31 +1,34 @@
 import Data.Numbers.Primes ( isPrime, primes )
-import Data.List ( subsequences )
 
 
 main :: IO ()
-main = print . solve $ candidates
-
-
-solve :: [[Int]] -> Int
-solve = minimum . map sum . filter isGoodList
+main = print . minimum . map sum $ candidates
 
 
 candidates :: [[Int]]
 candidates = do
+    let s0 = []
+
+    let candidates1 = takeWhile (<upperLimit) primes
     p1 <- candidates1
-    let s1 = [p1]
+    let s1 = p1:s0
+
     let candidates2 = filter (allPrimes s1) $ dropWhile (<= p1) candidates1
     p2 <- candidates2
     let s2 = p2:s1
+
     let candidates3 = filter (allPrimes s2) $ dropWhile (<= p2) candidates2
     p3 <- candidates3
     let s3 = p3:s2
+
     let candidates4 = filter (allPrimes s3) $ dropWhile (<= p3) candidates3
     p4 <- candidates4
     let s4 = p4:s3
+
     let candidates5 = filter (allPrimes s4) $ dropWhile (<= p4) candidates4
     p5 <- candidates5
     let s5 = p5:s4
+
     return s5
 
 
@@ -40,20 +43,8 @@ allPrimes (p:ps) q
         qp = concat' q p
 
 
-isGoodList :: [Int] -> Bool
-isGoodList = all (\[p, q] -> isPrime (concat' p q) && isPrime (concat' q p)) . combinations 2
-
-
-combinations :: Int -> [a] -> [[a]]
-combinations k = filter ((== k) . length) . subsequences
-
-
 concat' :: Int -> Int -> Int
 concat' p q = read $ shows p $ show q
-
-
-candidates1 :: [Int]
-candidates1 = takeWhile (<upperLimit) primes
 
 
 upperLimit :: Int
