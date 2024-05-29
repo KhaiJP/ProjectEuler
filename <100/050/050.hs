45259001cf155f3e6f4f09039fb17d7534c0ecbb
Khai <storage27592@gmail.com>
2024-05-29 12:33:16 +0900

    Update 050.hs

--- <100/050/050.hs
+++ <100/050/050.hs
@@ -1,17 +1,31 @@
 import Data.Numbers.Primes
 
+
 main :: IO ()
-main = print $ snd . maximum $ [(k - j, pk - pj) |
-                                  k <- [1 .. (len - 1)],
-                                  let pk = ps !! k,
-                                  j <- [1 .. k],
-                                  let pj = ps !! j,
-                                  isPrime (pk - pj)]
+main = print . snd . maximum . filter (\(c, p) -> isPrime p) $ consecutives
+
+
+consecutives :: [(Int, Int)]
+consecutives = do
+                k <- [1..(len-1)]
+                let pk = ps!!k
+                let j = myHead . filter (\j -> isPrime (pk - ps!!j)) $ [1..k]
+                let pj = if j > 1 then ps!!j else -100000000
+                return (k-j, pk-pj)
+
+
+myHead :: [Int] -> Int
+myHead [] = -1
+myHead (x:xs) = x
 
 
 len :: Int
 len = length ps
+
+
 ps :: [Int]
 ps = takeWhile (< upperLimit) . scanl1 (+) $ primes
+
+
 upperLimit :: Int
 upperLimit = 10^6
