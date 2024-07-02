5f6bb9585715b0f18dc88550d00b7c6838b28bc2
Khai <storage27592@gmail.com>
2024-07-02 20:00:11 +0900

    Solved 116 in Haskell

---
+++ <200/116/116.hs
@@ -0,0 +1,40 @@
+import qualified Data.Map.Strict as M ( Map, update, (!), fromList )
+type Row      = M.Map Position Int
+type Position = Int
+type Width    = Int
+
+
+main :: IO ()
+main = print . sum . map (M.! maxWidth) $ finalMaps
+
+
+finalMaps :: [Row]
+finalMaps = map (foldl update' initRow . makeCandidates) tiles
+
+
+initRow :: Row
+initRow = M.fromList [(n, 0) | n <- [0..maxWidth]]
+
+
+update' :: Row -> (Position, Width) -> Row
+update' row (x, w) = row'
+    where
+        row' = M.update (\a -> Just (a+p+q)) x row
+        p    = if x - w >= 0
+                    then row M.! (x-w) + 1
+                    else 0
+        q    = if x - 1 >= 0
+                    then row M.! (x-1)
+                    else 0
+
+
+makeCandidates :: Width -> [(Position, Width)]
+makeCandidates w = [(n, w) | n <- [1..maxWidth]]
+
+
+tiles :: [Width]
+tiles = [2, 3, 4]
+
+
+maxWidth :: Width
+maxWidth = 50
