d9e6bb120ffc3f6e1d7dc4cdfbd676f7e0af45d1
Khai <storage27592@gmail.com>
2024-07-13 15:05:25 +0900

    wrote a procedure of 131 and fixed variable name  in 131.hs

--- <200/131/131.hs
+++ <200/131/131.hs
@@ -2,4 +2,4 @@ import Data.Numbers.Primes ( isPrime )
 
 
 main :: IO ()
-main = print . length . filter isPrime . takeWhile (<10^6) $ [(n+1)^3 - n^3 | n <- [1..]]
+main = print . length . filter isPrime . takeWhile (<10^6) $ [(l+1)^3 - l^3 | l <- [1..]]
