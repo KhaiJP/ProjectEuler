c855709597aa6de4156077b85bb5a2e67bc9e635
Khai <storage27592@gmail.com>
2024-07-13 11:01:59 +0900

    Update 131.hs

--- <200/131/131.hs
+++ <200/131/131.hs
@@ -2,12 +2,4 @@ import Data.Numbers.Primes ( isPrime )
 
 
 main :: IO ()
-main  = print . length $ do
-        a <- [1..600]
-        let l = a^3
-        b <- filter (\b -> let u = b^3 in let gap = u - l in
-                        gap < 10^6 &&
-                        l `mod` gap /= 0 &&
-                        isPrime gap
-                    ) [(a+1)..601]
-        return 1
+main = print . length . filter isPrime . takeWhile (<10^6) $ [(n+1)^3 - n^3 | n <- [1..]]
