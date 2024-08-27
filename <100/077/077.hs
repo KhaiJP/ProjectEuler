276446f71bb366fee818e94c5088b9aa1c2a92c4
Khai <storage27592@gmail.com>
2024-08-27 19:57:26 +0900

    Update 077.hs

--- <100/077/077.hs
+++ <100/077/077.hs
@@ -8,12 +8,12 @@ main = print . head . filter achievedTarged $ [2..]
 primePartition :: Int -> Int -> Int
 primePartition 0 _ = 1
 primePartition _ 0 = 0
-primePartition n k = memo !! (k-1) !! n  + if isPrime k && k <= n
-                                            then memo !! k !! (n-k)
+primePartition n k = memo !! n !! (k-1)  + if isPrime k && k <= n
+                                            then memo !! (n-k) !! k
                                             else 0
 
 memo :: [[Int]]
-memo = [ [primePartition n k | n <- [0..] ]  | k <- [0..] ]
+memo = [ [primePartition n k | k <- [0..] ]  | n <- [0..] ]
 
 
 achievedTarged :: Int -> Bool
